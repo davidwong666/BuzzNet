f3be5ad09c44daf9e0d343491461fdbc1809a966
KsJackko <jackkozhang@gmail.com>
2025-05-11 19:51:53 +0800

    feat: Add administrator mode function - Implement administrator delete post permission - Add administrator mode logo

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -48,4 +48,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { protect };
+// Middleware to check if user is admin
+const isAdmin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403);
+    throw new Error('Not authorized as admin');
+  }
+});
+
+module.exports = { protect, isAdmin };
